e3bbd2bbaec55addb8f32673d5797c0c7c304471
Billy Tseng <tseng.billy@gmail.com>
2018-04-11 16:53:37 -0700

    feat: Add Binary.

---
+++ src/AddBinary.java
@@ -0,0 +1,30 @@
+/*Given two binary strings, return their sum (also a binary string).*/
+
+public class AddBinary {
+    /**
+     * @param a: a number
+     * @param b: a number
+     * @return: the result
+     */
+    public String addBinary(String a, String b) {
+        // write your code here
+        String ans = "";
+
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int sum = 0, carry = 0;
+        while (i >= 0 || j >=0) {
+            sum = carry;
+            sum += (i >= 0) ? a.charAt(i) - '0' : 0;
+            sum += (j >= 0) ? b.charAt(j) - '0' : 0;
+            carry = sum / 2;
+            ans = (sum % 2) + ans;
+            i--;
+            j--;
+        }
+        if (carry != 0) {
+            ans = carry + ans;
+        }
+        return ans;
+    }
+}
