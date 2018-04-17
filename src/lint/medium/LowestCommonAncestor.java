e3a95b779af8060194a4bab5dcb0044f02d60142
Billy Tseng <tseng.billy@gmail.com>
2018-04-16 20:10:02 -0700

    feat: lowest Common Ancestor.

---
+++ src/lint/medium/LowestCommonAncestor.java
@@ -0,0 +1,31 @@
+package lint.medium;
+
+public class LowestCommonAncestor {
+    /*
+     * @param root: The root of the binary search tree.
+     * @param A: A TreeNode in a Binary.
+     * @param B: A TreeNode in a Binary.
+     * @return: Return the least common ancestor(LCA) of the two nodes.
+     */
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode A, TreeNode B) {
+        // write your code here
+        if (root == null || root == A || root == B) {
+            return root;
+        }
+
+        TreeNode leftNode = lowestCommonAncestor(root.left, A, B);
+        TreeNode rightNode = lowestCommonAncestor(root.right, A, B);
+
+        if (leftNode != null && rightNode != null) {
+            return root;
+        }
+
+        if (leftNode != null) {
+            return leftNode;
+        }
+        if (rightNode != null) {
+            return rightNode;
+        }
+        return null;
+    }
+}
