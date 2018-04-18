bff554533760d6ffcc425addf406846a5fb428f1
Billy Tseng <tseng.billy@gmail.com>
2018-04-18 16:48:37 -0700

    feat: Roman To Integer.

---
+++ src/lint/medium/RomanToInteger.java
@@ -0,0 +1,35 @@
+package lint.medium;
+
+import java.util.HashMap;
+
+public class RomanToInteger {
+    /**
+     * @param s: Roman representation
+     * @return: an integer
+     */
+    public int romanToInt(String s) {
+        // write your code here
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+
+        HashMap<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        int sum = map.get(s.charAt(s.length() - 1));
+        for (int i = s.length() - 2; i >= 0; i--) {
+            if (map.get(s.charAt(i)) < map.get(s.charAt(i + 1))) {
+                sum -= map.get(s.charAt(i));
+            } else {
+                sum += map.get(s.charAt(i));
+            }
+        }
+        return sum;
+    }
+}
