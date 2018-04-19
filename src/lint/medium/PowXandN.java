99915d8569d3149e23373073f96b0f1ef4bc678f
Billy Tseng <tseng.billy@gmail.com>
2018-04-19 14:05:36 -0700

    feat: Pow(x, n).

---
+++ src/lint/medium/PowXandN.java
@@ -0,0 +1,23 @@
+package lint.medium;
+
+public class PowXandN {
+    /*
+     * @param x: the base number
+     * @param n: the power number
+     * @return: the result
+     */
+    public double myPow(double x, int n) {
+        // write your code here
+        if (n == 0) {
+            return 1.0;
+        }
+        double half = myPow(x, n/2);
+        if (n % 2 == 0) {
+            return half * half;
+        } else if (n > 0) {
+            return half * half * x;
+        } else {
+            return half * half / x;
+        }
+    }
+}
