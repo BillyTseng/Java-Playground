987edae2d4bbc6af220d7d6c2e3a10ae1d186dd8
Billy Tseng <tseng.billy@gmail.com>
2018-04-19 12:40:36 -0700

    feat: Best Time to Buy and Sell Stock.

---
+++ src/lint/medium/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,21 @@
+package lint.medium;
+
+public class BestTimeToBuyAndSellStock {
+    /**
+     * @param prices: Given an integer array
+     * @return: Maximum profit
+     */
+    public int maxProfit(int[] prices) {
+        // write your code here
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+        int profit = 0;
+        int min = Integer.MAX_VALUE;
+        for (int price : prices) {
+            min = Math.min(min, price);
+            profit = Math.max(profit, price - min);
+        }
+        return profit;
+    }
+}
