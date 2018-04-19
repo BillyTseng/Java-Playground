c2ee7e6b3ba13724aa6ea226f7ce768a01d0b45e
Billy Tseng <tseng.billy@gmail.com>
2018-04-18 17:54:26 -0700

    feat: Valid Parentheses.

---
+++ src/lint/easy/ValidParentheses.java
@@ -0,0 +1,37 @@
+package lint.easy;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class ValidParentheses {
+    /**
+     * @param s: A string
+     * @return: whether the string is a valid parentheses
+     */
+    public boolean isValidParentheses(String s) {
+        // write your code here
+        if (s == null) {
+            return false;
+        }
+        Deque<Character> stack = new LinkedList<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            }
+            if (c == ')') {
+                if (stack.isEmpty() || stack.pop() != '(') {
+                    return false;
+                }
+            } else if (c == ']') {
+                if (stack.isEmpty() || stack.pop() != '[') {
+                    return false;
+                }
+            } else if (c == '}') {
+                if (stack.isEmpty() || stack.pop() != '{') {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
