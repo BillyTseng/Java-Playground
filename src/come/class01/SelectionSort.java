b969f1396997d5bf9510900caf646fcf848adf12
Billy Tseng <tseng.billy@gmail.com>
2018-04-21 21:07:03 -0700

    feat: Selection Sort.

---
+++ src/come/class01/SelectionSort.java
@@ -0,0 +1,27 @@
+package come.class01;
+
+public class SelectionSort {
+    public int[] selectionSort(int[] array) {
+        // Write your solution here
+        if (array == null || array.length <= 0) {
+            return array;
+        }
+
+        for (int i = 0; i < array.length - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[min]) {
+                    min = j;
+                }
+            }
+            swap(array, i, min);
+        }
+        return array;
+    }
+
+    private void swap(int[] array, int a, int b) {
+        int temp = array[a];
+        array[a] = array[b];
+        array[b] = temp;
+    }
+}
