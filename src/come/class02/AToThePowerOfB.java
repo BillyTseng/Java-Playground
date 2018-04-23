34518ab2e5e7e97861006bfc2fd57441bc579bd7
Billy Tseng <tseng.billy@gmail.com>
2018-04-23 16:36:21 -0700

    feat: A To The Power Of B.

---
+++ src/come/class02/AToThePowerOfB.java
@@ -0,0 +1,29 @@
+package come.class02;
+
+public class AToThePowerOfB {
+    public long power(int a, int b) {
+        // Write your solution here
+        if (b == 0) {
+            return 1;
+        }
+        if (a == 0) {
+            return 0;
+        }
+        long half = power(a, b / 2);
+        if (b % 2 == 0) {
+            return half * half;
+        } else if (b > 0) {
+            return half * half * a;
+        } else {
+            return half * half / a;
+        }
+    }
+
+    public static void main(String[] args) {
+        AToThePowerOfB solution = new AToThePowerOfB();
+        System.out.println(solution.power(0, 0));
+        System.out.println(solution.power(0, 100));
+        System.out.println(solution.power(2, 3));
+        System.out.println(solution.power(3, 3));
+    }
+}
