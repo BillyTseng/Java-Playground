2924ea656ab9cbcb11a912ee8bf025b6fcd815f9
Billy Tseng <tseng.billy@gmail.com>
2018-04-27 14:18:07 -0700

    feat: Reverse LinkedList.

---
+++ src/come/class03/ReverseLinkedList.java
@@ -0,0 +1,27 @@
+package come.class03;
+
+class ListNode {
+    public int value;
+    public ListNode next;
+
+    public ListNode(int value) {
+        this.value = value;
+        next = null;
+    }
+}
+
+public class ReverseLinkedList {
+    public ListNode reverse(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode prev = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+}
