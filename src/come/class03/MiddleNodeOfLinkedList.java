505708b15974571b893ab0d2a53dc947478bcc2f
Billy Tseng <tseng.billy@gmail.com>
2018-04-27 21:42:30 -0700

    feat: Chose the Second Middle Node Of Linked List While the numbers of node is Even.

--- src/come/class03/MiddleNodeOfLinkedList.java
+++ src/come/class03/MiddleNodeOfLinkedList.java
@@ -1,7 +1,7 @@
 package come.class03;
 
 public class MiddleNodeOfLinkedList {
-    public ListNode middleNode(ListNode head) {
+    private ListNode middleNode(ListNode head) {
         if (head == null) {
             return head;
         }
@@ -11,5 +11,34 @@ public class MiddleNodeOfLinkedList {
             slow = slow.next;
         }
         return slow;
+    }
+
+    private ListNode middleNodeChoseSecondWhileEven(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode fast = head, slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+
+    public static void main(String[] args) {
+        ListNode n1 = new ListNode(1);
+        ListNode n2 = new ListNode(2);
+        ListNode n3 = new ListNode(3);
+        ListNode n4 = new ListNode(4);
+
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        MiddleNodeOfLinkedList solution = new MiddleNodeOfLinkedList();
+        // 1->2->3->4, middle = 2
+        System.out.println(solution.middleNode(n1).value);
+        // 1->2->3->4, middle = 3
+        System.out.println(solution.middleNodeChoseSecondWhileEven(n1).value);
+
     }
 }
