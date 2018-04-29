9f38d5fda5fb3e46c6885597a1b85f60c96c161a
Billy Tseng <tseng.billy@gmail.com>
2018-04-29 14:55:23 -0700

    chore: Sort With 3 Stacks.

---
+++ src/come/class03/SortWith3Stacks.java
@@ -0,0 +1,51 @@
+package come.class03;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class SortWith3Stacks {
+    private void sort(LinkedList<Integer> s1) {
+        LinkedList<Integer> s2 = new LinkedList<>();
+        LinkedList<Integer> s3 = new LinkedList<>();
+
+        int s1Size = s1.size();
+        while (s3.size() < s1Size) {
+            int min = Integer.MAX_VALUE;
+            while (!s1.isEmpty()) {
+                int s1Elem = s1.pop();
+                if (s1Elem <= min) {
+                    min = s1Elem;
+                }
+                s2.push(s1Elem);
+            }
+
+            while (!s2.isEmpty()) {
+                int s2Elem = s2.pop();
+                if (s2Elem > min) {
+                    s1.push(s2Elem);
+                } else {
+                    s3.push(min);
+                }
+            }
+        }
+
+        while (!s3.isEmpty()) {
+            s1.push(s3.pop());
+        }
+    }
+
+
+    public static void main(String[] args) {
+        SortWith3Stacks solution = new SortWith3Stacks();
+        LinkedList<Integer> s1 = new LinkedList<>();
+        s1.push(4);
+        s1.push(4);
+        s1.push(3);
+        s1.push(5);
+        s1.push(-1);
+
+        System.out.println(Arrays.toString(s1.toArray()));
+        solution.sort(s1);
+        System.out.println(Arrays.toString(s1.toArray()));
+    }
+}
