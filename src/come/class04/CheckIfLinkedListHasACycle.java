2f5ecf73968f0dc42fe495350e75288eee83ac32
Billy Tseng <tseng.billy@gmail.com>
2018-04-30 13:35:21 -0700

    chore: Check If Linked List Has A Cycle.

---
+++ src/come/class04/CheckIfLinkedListHasACycle.java
@@ -0,0 +1,18 @@
+package come.class04;
+
+public class CheckIfLinkedListHasACycle {
+    public boolean hasCycle(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+        ListNode fast = head, slow = head;
+        while (fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast.equals(slow)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
