1db177ae8b7dfddadb9370fad9bb0134adb1edab
Billy Tseng <tseng.billy@gmail.com>
2018-05-05 11:24:48 -0700

    feat: fast way to Check If Binary Tree Is Balanced.

--- src/come/class05/CheckIfBinaryTreeIsBalanced.java
+++ src/come/class05/CheckIfBinaryTreeIsBalanced.java
@@ -16,17 +16,30 @@ public class CheckIfBinaryTreeIsBalanced {
             return true;
         }
 
-        if (Math.abs(getHeight(root.left) - getHeight(root.right)) > 1) {
+        if (-1 == getHeight(root)) {
             return false;
         }
 
-        return isBalanced(root.left) && isBalanced(root.right);
+        return true;
     }
 
+    /**
+     * @param root: A root node of a tree
+     * @return: If the differ of heights is greater than 1, return -1. If not, return height
+     */
     private int getHeight(TreeNode root) {
         if (root == null) {
             return 0;
         }
-        return Math.max(getHeight(root.left), getHeight(root.right)) + 1;
+        int leftHeight = getHeight(root.left);
+        int rightHeight = getHeight(root.right);
+
+        if (leftHeight == -1 || rightHeight == -1) {
+            return -1;
+        }
+        if (Math.abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        }
+        return Math.max(leftHeight, rightHeight) + 1;
     }
 }
