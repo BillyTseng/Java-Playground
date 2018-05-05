33c9493fff62129368fb63714ac35436f536bced
Billy Tseng <tseng.billy@gmail.com>
2018-05-05 11:05:48 -0700

    feat: Check If Binary Tree Is Balanced.

---
+++ src/come/class05/CheckIfBinaryTreeIsBalanced.java
@@ -0,0 +1,32 @@
+package come.class05;
+
+class TreeNode {
+    public int key;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int key) {
+        this.key = key;
+    }
+}
+
+public class CheckIfBinaryTreeIsBalanced {
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+
+        if (Math.abs(getHeight(root.left) - getHeight(root.right)) > 1) {
+            return false;
+        }
+
+        return isBalanced(root.left) && isBalanced(root.right);
+    }
+
+    private int getHeight(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return Math.max(getHeight(root.left), getHeight(root.right)) + 1;
+    }
+}
