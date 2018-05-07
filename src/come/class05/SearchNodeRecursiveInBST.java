b80c8ea34ef577ce2474ef8fa715b146c19678dd
Billy Tseng <tseng.billy@gmail.com>
2018-05-07 09:30:02 -0700

    feat: Search Node Recursively In BST.

---
+++ src/come/class05/SearchNodeRecursiveInBST.java
@@ -0,0 +1,13 @@
+package come.class05;
+
+public class SearchNodeRecursiveInBST {
+    private TreeNode search(TreeNode root, int target) {
+        if (root == null || root.key == target) {
+            return root;
+        } else if (root.key < target) {
+            return search(root.right, target);
+        } else {
+            return search(root.left, target);
+        }
+    }
+}
