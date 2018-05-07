9b31da77f2291ca1fe1630fb371f3356cf739776
Billy Tseng <tseng.billy@gmail.com>
2018-05-07 10:44:47 -0700

    feat: revised insert node recursively.

--- src/come/class05/InsertNodeRecursiveInBST.java
+++ src/come/class05/InsertNodeRecursiveInBST.java
@@ -6,11 +6,10 @@ public class InsertNodeRecursiveInBST {
             return new TreeNode(key);
         }
         if (key < root.key) {
-            return insert(root.left, key);
-        } else if (root.key > key) {
-            return insert(root.right, key);
-        } else {
-            return root;
+            root.left = insert(root.left, key);
+        } else if (root.key < key) {
+            root.right = insert(root.right, key);
         }
+        return root;
     }
 }
