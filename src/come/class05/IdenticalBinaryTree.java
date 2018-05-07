ce0a40657067e4ae650ea5647828f1976738de8b
Billy Tseng <tseng.billy@gmail.com>
2018-05-07 14:24:58 -0700

    feat: Identical Binary Tree.

---
+++ src/come/class05/IdenticalBinaryTree.java
@@ -0,0 +1,15 @@
+package come.class05;
+
+public class IdenticalBinaryTree {
+    private boolean isIdentical(TreeNode one, TreeNode two) {
+        if (one == null && two == null) {
+            return true;
+        } else if (one == null || two == null) {
+            return false;
+        } else if (one.key != two.key) {
+            return false;
+        } else {
+            return isIdentical(one.left, two.left) && isIdentical(one.right, two.right);
+        }
+    }
+}
