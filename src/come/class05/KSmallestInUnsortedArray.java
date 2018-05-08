fb1dd2dfddb133f64775095a75d439f18310fa81
Billy Tseng <tseng.billy@gmail.com>
2018-05-08 16:50:54 -0700

    feat: K Smallest In Unsorted Array By Max Heap.

---
+++ src/come/class05/KSmallestInUnsortedArray.java
@@ -0,0 +1,27 @@
+package come.class05;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class KSmallestInUnsortedArray {
+    public int[] kSmallestByMaxHeap(int[] array, int k) {
+        if (array == null || array.length <= 0 || k == 0) {
+            return new int[0];
+        }
+        Queue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        for (int i = 0; i < array.length; i++) {
+            if (i < k) {
+                maxHeap.offer(array[i]);
+            } else if (array[i] < maxHeap.peek()) {
+                maxHeap.poll();
+                maxHeap.offer(array[i]);
+            }
+        }
+        int[] res = new int[k];
+        for (int i = k - 1; i >= 0; i--) {
+            res[i] = maxHeap.poll();
+        }
+        return res;
+    }
+}
