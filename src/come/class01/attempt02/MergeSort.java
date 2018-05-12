44ccb5cc25ea8b6b4655d83394c99eed54294805
Billy Tseng <tseng.billy@gmail.com>
2018-05-12 14:45:39 -0700

    feat: Merge Sort attempt 02.

---
+++ src/come/class01/attempt02/MergeSort.java
@@ -0,0 +1,40 @@
+package come.class01.attempt02;
+
+public class MergeSort {
+    public int[] mergeSort(int[] array) {
+        if (array == null || array.length <= 1) {
+            return array;
+        }
+        int[] helper = new int[array.length];
+        mergeSort(array, helper, 0, array.length - 1);
+        return array;
+    }
+
+    private void mergeSort(int[] array, int[] helper, int left, int right) {
+        if (left >= right) {
+            return;
+        }
+        int mid = left + (right - left) / 2;
+        mergeSort(array, helper, left, mid);
+        mergeSort(array, helper, mid + 1, right);
+        merge(array, helper, left, right);
+    }
+
+    private void merge(int[] array, int[] helper, int left, int right) {
+        for (int i = left; i <= right; i++) {
+            helper[i] = array[i];
+        }
+        int mid = left + (right - left) / 2;
+        int i = left, j = mid + 1, idx = left;
+        while (i <= mid && j <= right) {
+            if (helper[j] < helper[i]) {
+                array[idx++] = helper[j++];
+            } else {
+                array[idx++] = helper[i++];
+            }
+        }
+        while (i <= mid) {
+            array[idx++] = helper[i++];
+        }
+    }
+}
