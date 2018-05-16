05f05e3ac3de8bad2542b8a181ac887ae11447d3
Billy Tseng <tseng.billy@gmail.com>
2018-05-16 16:49:02 -0700

    feat: Closest In Sorted Array attempt 02.

---
+++ src/come/class02/attempt02/ClosestInSortedArray.java
@@ -0,0 +1,21 @@
+package come.class02.attempt02;
+
+public class ClosestInSortedArray {
+    public int closest(int[] array, int target) {
+        if (array == null || array.length == 0) {
+            return -1;
+        }
+        int left = 0, right = array.length - 1;
+        while (left < right - 1) {
+            int mid = left + (right - left) / 2;
+            if (target <= array[mid]) {
+                right = mid;
+            } else {
+                left = mid;
+            }
+        }
+        int leftDiff = target - array[left];
+        int rightDiff = array[right] - target;
+        return (leftDiff <= rightDiff) ? left : right;
+    }
+}
