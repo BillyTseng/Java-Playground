406187da11c4cf1a551bb4e93a2148d0ebfe1099
Billy Tseng <tseng.billy@gmail.com>
2018-05-20 11:39:18 -0700

    feat: Reverse String.

---
+++ src/come/class08/attempt01/ReverseString.java
@@ -0,0 +1,38 @@
+package come.class08.attempt01;
+
+public class ReverseString {
+    public String reverseIterative(String input) {
+        if (input == null) {
+            return null;
+        }
+        char[] array = input.toCharArray();
+        int i = 0, j = array.length - 1;
+        while (i <= j) {
+            swap(array, i++, j--);
+        }
+        return new String(array);
+    }
+
+    public String reverseRecursive(String input) {
+        if (input == null) {
+            return null;
+        }
+        char[] array = input.toCharArray();
+        helper(array, 0, array.length - 1);
+        return new String(array);
+    }
+
+    private void helper(char[] array, int i, int j) {
+        if (i >= j) {
+            return;
+        }
+        swap(array, i, j);
+        helper(array, ++i, --j);
+    }
+
+    private void swap(char[] array, int a, int b) {
+        char temp = array[a];
+        array[a] = array[b];
+        array[b] = temp;
+    }
+}
