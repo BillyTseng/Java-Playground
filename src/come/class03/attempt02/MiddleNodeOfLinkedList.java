5bc917bd4a662d0b9c1ffffd548c5ee3bc90e4a9
Billy Tseng <tseng.billy@gmail.com>
2018-05-23 16:59:07 -0700

    feat: Middle Node Of Linked List attempt 02.

---
+++ src/come/class03/attempt02/MiddleNodeOfLinkedList.java
@@ -0,0 +1,16 @@
+package come.class03.attempt02;
+
+public class MiddleNodeOfLinkedList {
+    public ListNode middleNode(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
