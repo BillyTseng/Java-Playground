944c9b568b6a9dc11e58280d8775c69210809b72
Billy Tseng <tseng.billy@gmail.com>
2018-06-03 13:37:29 -0700

    feat: Max Product Of Cutting Rope(DP).

--- src/come/class12/attempt01/MaxProductOfCuttingRope.java
+++ src/come/class12/attempt01/MaxProductOfCuttingRope.java
@@ -1,6 +1,23 @@
 package come.class12.attempt01;
 
 public class MaxProductOfCuttingRope {
+    public int maxProduct(int length) {
+        int[] stateArray = new int[length + 1];
+        stateArray[0] = 0;
+        stateArray[1] = 0;
+
+        for (int i = 1; i < stateArray.length; i++) {
+            int max = 0;
+            for (int j = 1; j < i; j++) {
+                max = Math.max(max, Math.max(j, stateArray[j]) * (i - j));
+            }
+            stateArray[i] = max;
+        }
+        return stateArray[length];
+    }
+
+
+
     private int maxProductI(int length) {
         if (length <= 1) {
             return 0;
