bc21cb9e16ad3aad6dadf736f617b59362fc46dd
Billy Tseng <tseng.billy@gmail.com>
2018-06-06 21:37:35 -0700

    feat: Push Dominoes.

---
+++ src/leet/dynamicProgramming/PushDominoes.java
@@ -0,0 +1,41 @@
+package leet.dynamicProgramming;
+
+/* 838. Push Dominoes - medium */
+
+public class PushDominoes {
+    public String pushDominoes(String dominoes) {
+        char[] chars = dominoes.toCharArray();
+        final int N = chars.length;
+        int[] forces = new int[N];
+
+        int force = 0;
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] == 'R') {
+                force = N;
+            } else if (chars[i] == 'L') {
+                force = 0;
+            } else {
+                force = Math.max(force - 1, 0);
+            }
+            forces[i] += force;
+        }
+
+        force = 0;
+        for (int j = chars.length - 1; j >= 0; j--) {
+            if (chars[j] == 'R') {
+                force = 0;
+            } else if (chars[j] == 'L') {
+                force = N;
+            } else {
+                force = Math.max(force - 1, 0);
+            }
+            forces[j] -= force;
+        }
+
+        StringBuilder ans = new StringBuilder();
+        for (int f : forces) {
+            ans.append(f > 0 ? 'R' : f < 0 ? 'L' : '.');
+        }
+        return ans.toString();
+    }
+}
