2e41a5d28a06eb4c4a381e63d8326927569273e9
Billy Tseng <tseng.billy@gmail.com>
2018-06-06 21:36:47 -0700

    feat: Largest Sum of Averages.

---
+++ src/leet/dynamicProgramming/LargestSumOfAverages.java
@@ -0,0 +1,24 @@
+package leet.dynamicProgramming;
+
+/* 813. Largest Sum of Averages - medium */
+
+public class LargestSumOfAverages {
+    public double largestSumOfAverages(int[] A, int K) {
+        int n = A.length;
+        double[][] dp = new double[K + 1][n + 1];
+        double[] sum = new double[n + 1];
+        for (int i = 1; i <= n; i++) {
+            sum[i] = sum[i - 1] + A[i - 1];
+            dp[1][i] = sum[i] / i;
+        }
+
+        for (int k = 2; k <= K; k++) {
+            for (int i = k; i <= n; i++) {
+                for (int j = k - 1; j < i; j++) {
+                    dp[k][i] = Math.max(dp[k][i], dp[k - 1][j] + (sum[i] - sum[j]) / (i - j));
+                }
+            }
+        }
+        return dp[K][n];
+    }
+}
