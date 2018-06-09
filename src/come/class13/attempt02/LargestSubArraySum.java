0dd47cb3393dadb462888997607e5d49ea230a8e
Billy Tseng <tseng.billy@gmail.com>
2018-06-08 21:41:53 -0700

    feat: Largest Sub Array Sum attempt 02.

---
+++ src/come/class13/attempt02/LargestSubArraySum.java
@@ -0,0 +1,20 @@
+package come.class13.attempt02;
+
+public class LargestSubArraySum {
+    public int largestSum(int[] array) {
+        if (array == null || array.length == 0) {
+            return 0;
+        }
+        int max = array[0];
+        int prevSum = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (prevSum < 0) {
+                prevSum = array[i];
+            } else {
+                prevSum += array[i];
+            }
+            max = Math.max(max, prevSum);
+        }
+        return max;
+    }
+}
