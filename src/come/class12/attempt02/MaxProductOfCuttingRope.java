9550d8e36551a2be0d53ff59763aa51324dae0dd
Billy Tseng <tseng.billy@gmail.com>
2018-06-08 21:41:14 -0700

    feat: Max Product Of Cutting Rope attempt 02.

---
+++ src/come/class12/attempt02/MaxProductOfCuttingRope.java
@@ -0,0 +1,17 @@
+package come.class12.attempt02;
+
+public class MaxProductOfCuttingRope {
+    public int maxProduct(int length) {
+        int[] m = new int[length + 1];
+        m[1] = 0;
+        for (int i = 2; i < m.length; i++) {
+            int max = 0;
+            for (int j = 1; j < i; j++) {
+                int maxProduct = Math.max(j, m[j]) * (i - j);
+                max = Math.max(max, maxProduct);
+            }
+            m[i] = max;
+        }
+        return m[length];
+    }
+}
