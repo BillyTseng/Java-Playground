45f9911204195f31474c55ca4c34cd0ea5c225cf
Billy Tseng <tseng.billy@gmail.com>
2018-06-09 14:27:16 -0700

    feat: Largest Square Of 1s attempt 02.
    line 14: "} else if (matrix[i][j] == 1) {"

---
+++ src/come/class13/attempt02/LargestSquareOf1s.java
@@ -0,0 +1,24 @@
+package come.class13.attempt02;
+
+public class LargestSquareOf1s {
+    public int largest(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int max = 0;
+        int[][] squares = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == 0) {
+                    squares[i][j] = matrix[i][j];
+                } else if (matrix[i][j] == 1) {
+                    squares[i][j] = Math.min(squares[i - 1][j], squares[i][j - 1]);
+                    squares[i][j] = Math.min(squares[i - 1][j - 1], squares[i][j]);
+                    squares[i][j] += 1;
+                }
+                max = Math.max(max, squares[i][j]);
+            }
+        }
+        return max;
+    }
+}
