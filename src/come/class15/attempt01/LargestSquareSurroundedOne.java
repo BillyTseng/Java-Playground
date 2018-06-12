369583e26c6a0263b3bbd9265a7dbfe3dfb02e1e
Billy Tseng <tseng.billy@gmail.com>
2018-06-11 21:07:35 -0700

    feat: Largest Square Surrounded One I.

--- src/come/class15/attempt01/LargestSquareSurroundedOne.java
+++ src/come/class15/attempt01/LargestSquareSurroundedOne.java
@@ -2,6 +2,7 @@ package come.class15.attempt01;
 
 // Can't find the online assessment.
 public class LargestSquareSurroundedOne {
+    // follow lecture and implement by my own.
     private int largest(int[][] matrix) {
         int m = matrix.length;
         if (m == 0) {
@@ -66,9 +67,40 @@ public class LargestSquareSurroundedOne {
         return res;
     }
 
+    // from solution
+    private int largestI(int[][] matrix) {
+        if (matrix.length == 0 || matrix[0].length == 0) {
+            return 0;
+        }
+        int result = 0;
+        int M = matrix.length;
+        int N = matrix[0].length;
+
+        int left[][] = new int[M + 1][N + 1];
+        int up[][] = new int[M + 1][N + 1];
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+                if (matrix[i][j] == 1) {
+                    left[i + 1][j + 1] = left[i + 1][j] + 1;
+                    up[i + 1][j + 1] = up[i][j + 1] + 1;
+
+                    for (int maxLength = Math.min(left[i + 1][j + 1], up[i + 1][j + 1]); maxLength >= 1; maxLength--) {
+                        if (left[(i + 1 - maxLength) + 1][j + 1] >= maxLength &&
+                                up[i + 1][(j + 1 - maxLength) + 1] >= maxLength) {
+                            result = Math.max(result, maxLength);
+                            break;
+                        }
+                    }
+                }
+            }
+
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         LargestSquareSurroundedOne solution = new LargestSquareSurroundedOne();
-        System.out.println(solution.largest(new int[][]{ {1, 0, 1, 1, 1},
+        System.out.println(solution.largestI(new int[][]{ {1, 0, 1, 1, 1},
                                                          {1, 1, 1, 1, 1},
                                                          {1, 1, 0, 1, 0},
                                                          {1, 1, 1, 1, 1},
