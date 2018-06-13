6d1e2d6aad523607eb83b5ef981e22d57e41d1f7
Billy Tseng <tseng.billy@gmail.com>
2018-06-12 20:03:01 -0700

    feat: Generics type example 1.

---
+++ src/come/practice/class20/e01/generics1.java
@@ -0,0 +1,20 @@
+package come.practice.class20.e01;
+
+public class generics1 {
+    public static <E> void printArray(E[] inputArray) {
+        for (E element : inputArray) {
+            System.out.printf("%s ", element);
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Integer[] intArray = {1, 2, 3, 4, 5};
+        Double[] doubleArray = {1.1, 2.2, 3.3, 4.4, 5.5};
+        Character[] charArray = {'H', 'E', 'L', 'L', 'O'};
+
+        printArray(intArray);
+        printArray(doubleArray);
+        printArray(charArray);
+    }
+}
