68d83dab119bdbde887168c170dbf69c3b254550
Billy Tseng <tseng.billy@gmail.com>
2018-06-18 21:44:39 -0700

    feat: Spiral Order Traverse I.

---
+++ src/come/class10/attempt01/SpiralOrderTraverseI.java
@@ -0,0 +1,40 @@
+package come.class10.attempt01;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SpiralOrderTraverseI {
+    public List<Integer> spiral(int[][] matrix) {
+        List<Integer> res = new ArrayList<>();
+        spiral(matrix, 0, matrix.length, res);
+        return res;
+    }
+
+    private void spiral(int[][] matrix, int offset, int size, List<Integer> res) {
+        if (size == 0) {
+            return;
+        }
+
+        if (size == 1) {
+            res.add(matrix[offset][offset]);
+            return;
+        }
+
+        for (int i = 0; i < size - 1; i++) {
+            res.add(matrix[offset][offset + i]);
+        }
+
+        for (int i = 0; i < size - 1; i++) {
+            res.add(matrix[offset + i][offset + size - 1]);
+        }
+
+        for (int i = size - 1; i >= 1; i--) {
+            res.add(matrix[offset + size - 1][offset + i]);
+        }
+
+        for (int i = size - 1; i >= 1; i--) {
+            res.add(matrix[offset + i][offset]);
+        }
+        spiral(matrix, offset + 1, size - 2, res);
+    }
+}
