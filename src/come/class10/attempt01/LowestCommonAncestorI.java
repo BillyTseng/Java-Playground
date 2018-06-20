b74e1451bdea12bbf2b5c7a90430630eb06e25c2
Billy Tseng <tseng.billy@gmail.com>
2018-06-19 21:15:05 -0700

    feat: Lowest Common Ancestor I.

---
+++ src/come/class10/attempt01/LowestCommonAncestorI.java
@@ -0,0 +1,17 @@
+package come.class10.attempt01;
+
+public class LowestCommonAncestorI {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode one, TreeNode two) {
+        if (root == null || root == one || root == two) {
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left, one, two);
+        TreeNode right = lowestCommonAncestor(root.right, one, two);
+
+        if (left != null && right != null) {
+            return root;
+        }
+        return left == null ? right : left;
+    }
+}
