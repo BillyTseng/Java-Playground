f2784809e9cf5bbbbd60085c1da6973924625c70
Billy Tseng <tseng.billy@gmail.com>
2018-06-21 19:31:57 -0700

    feat: DP III attempt 03.

---
+++ src/come/class15/attempt03/LargestSubArraySumAndIndex.java
@@ -0,0 +1,25 @@
+package come.class15.attempt03;
+
+public class LargestSubArraySumAndIndex {
+    public int[] largestSum(int[] array) {
+        int left = 0, solLeft = 0, solRight = 0;
+        int prevSum = array[0];
+        int max = array[0];
+
+        for (int i = 1; i < array.length; i++) {
+            if (prevSum < 0) {
+                prevSum = array[i];
+                left = i;
+            } else {
+                prevSum += array[i];
+            }
+            int temp = max;
+            max = Math.max(max, prevSum);
+            if (temp != max) {
+                solLeft = left;
+                solRight = i;
+            }
+        }
+        return new int[] {max, solLeft, solRight};
+    }
+}
---
+++ src/come/class15/attempt03/LongestCrossOf1s.java
@@ -0,0 +1,66 @@
+package come.class15.attempt03;
+
+public class LongestCrossOf1s {
+    public int largest(int[][] matrix) {
+        int m = matrix.length;
+        if (m == 0) {
+            return 0;
+        }
+        int n = matrix[0].length;
+        if (n == 0) {
+            return 0;
+        }
+
+        int[][] leftUp = buildLeftUp(matrix, m, n);
+        int[][] rightDown = buildRightDown(matrix, m, n);
+        return merge(leftUp, rightDown, m, n);
+    }
+
+    private int getNumber(int[][] input, int i, int j, int m, int n) {
+        if (i < 0 || i >= m || j < 0 || j >= n) {
+            return 0;
+        }
+        return input[i][j];
+    }
+
+    private int merge(int[][] tar, int[][] src, int m, int n) {
+        int max = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                tar[i][j] = Math.min(tar[i][j], src[i][j]);
+                max = Math.max(max, tar[i][j]);
+            }
+        }
+        return max;
+    }
+
+    private int[][] buildLeftUp(int[][] matrix, int m, int n) {
+        int[][] left = new int[m][n];
+        int[][] up = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 1) {
+                    left[i][j] = getNumber(left, i, j - 1, m, n) + 1;
+                    up[i][j] = getNumber(up, i - 1, j, m, n) + 1;
+                }
+            }
+        }
+        merge(left, up, m, n);
+        return left;
+    }
+
+    private int[][] buildRightDown(int[][] matrix, int m, int n) {
+        int[][] right = new int[m][n];
+        int[][] down = new int[m][n];
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                if (matrix[i][j] == 1) {
+                    right[i][j] = getNumber(right, i, j + 1, m, n) + 1;
+                    down[i][j] = getNumber(down, i + 1, j, m, n) + 1;
+                }
+            }
+        }
+        merge(right, down, m, n);
+        return right;
+    }
+}
