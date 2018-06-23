421b66cd7d20a9c49972858d95c0631c734a298d
Billy Tseng <tseng.billy@gmail.com>
2018-06-23 10:11:18 -0700

    feat: Revised 2, Largest Square Surrounded One attempt 04.

--- src/come/class15/attempt04/LargestSquareSurroundedOne.java
+++ src/come/class15/attempt04/LargestSquareSurroundedOne.java
@@ -8,8 +8,8 @@ public class LargestSquareSurroundedOne {
         if (matrix.length == 0 || matrix[0].length == 0) {
             return 0;
         }
-        int m = matrix[0].length;
-        int n = matrix.length;
+        int m = matrix.length;
+        int n = matrix[0].length;
         int[][] left = new int[m + 1][n + 1];
         int[][] up = new int[m + 1][n + 1];
         int max = 0;
