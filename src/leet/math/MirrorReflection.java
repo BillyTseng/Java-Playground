a957bf4ec73bfca53509f7c4c13ae048b3f1202b
Billy Tseng <tseng.billy@gmail.com>
2018-06-29 21:48:15 -0700

    feat: Mirror Reflection.

---
+++ src/leet/math/MirrorReflection.java
@@ -0,0 +1,19 @@
+package leet.math;
+
+/* 858. Mirror Reflection - medium */
+
+public class MirrorReflection {
+    public int mirrorReflection(int p, int q) {
+        while (p % 2 == 0 && q % 2 == 0) {
+            p /= 2;
+            q /= 2;
+        }
+        if (p % 2 == 0) {
+            return 2;
+        } else if (q % 2 == 0) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+}
