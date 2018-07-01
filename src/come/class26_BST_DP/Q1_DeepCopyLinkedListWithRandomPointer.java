2b8116dbd71bf9b319db1bd573425a3afba9b03d
Billy Tseng <tseng.billy@gmail.com>
2018-06-30 21:45:33 -0700

    feat: Deep Copy Linked List With Random Pointer.

---
+++ src/come/class26_BST_DP/Q1_DeepCopyLinkedListWithRandomPointer.java
@@ -0,0 +1,41 @@
+package come.class26_BST_DP;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Q1_DeepCopyLinkedListWithRandomPointer {
+    class RandomListNode {
+        public int value;
+        public RandomListNode next;
+        public RandomListNode random;
+
+        RandomListNode(int value) {
+            this.value = value;
+        }
+    }
+
+    public RandomListNode copy(RandomListNode head) {
+        if (head == null) {
+            return null;
+        }
+        Map<RandomListNode, RandomListNode> lookup = new HashMap<>();
+        RandomListNode newHead = new RandomListNode(head.value);
+        RandomListNode curr = newHead;
+        lookup.put(head, newHead);
+
+        while (head != null) {
+            if (head.next != null) {
+                lookup.put(head.next, new RandomListNode(head.next.value));
+                curr.next = lookup.get(head.next);
+            }
+
+            if (head.random != null) {
+                lookup.put(head.random, new RandomListNode(head.random.value));
+                curr.random = lookup.get(head.random);
+            }
+            head = head.next;
+            curr = curr.next;
+        }
+        return newHead;
+    }
+}
