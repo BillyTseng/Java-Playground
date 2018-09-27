af3b9fcccc75c891e413cf00ab4d261c833e92eb
Billy Tseng <tseng.billy@gmail.com>
2018-09-26 22:30:51 -0700

    feat: algorithm problems.

---
+++ src/leet/a/Q141_LinkedListCycle.java
@@ -0,0 +1,31 @@
+package leet.a;
+
+public class Q141_LinkedListCycle {
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public boolean hasCycle(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
