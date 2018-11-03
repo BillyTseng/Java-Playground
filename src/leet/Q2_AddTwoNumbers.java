85e918ed1baae151933f2785b2ee6a94d89f90ce
Billy Tseng <tseng.billy@gmail.com>
2018-11-02 22:28:17 -0700

    feat: algorithm problems.

---
+++ src/leet/Q2_AddTwoNumbers.java
@@ -0,0 +1,35 @@
+package leet;
+
+public class Q2_AddTwoNumbers {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int carry = 0;
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        while (l1 != null || l2 != null) {
+            int sum = carry;
+            sum += (l1 == null) ? 0 : l1.val;
+            sum += (l2 == null) ? 0 : l2.val;
+
+            curr.next = new ListNode(sum % 10);
+            carry = sum / 10;
+
+            curr = curr.next;
+            l1 = (l1 == null) ? null : l1.next;
+            l2 = (l2 == null) ? null : l2.next;
+        }
+
+        if (carry > 0) {
+            curr.next = new ListNode(carry);
+        }
+        return dummy.next;
+    }
+}
