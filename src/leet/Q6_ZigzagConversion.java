9a851c3098b866df7598fb5124742ad7589eb3d3
Billy Tseng <tseng.billy@gmail.com>
2018-11-03 22:51:36 -0700

    feat: algorithm problems.

---
+++ src/leet/Q6_ZigzagConversion.java
@@ -0,0 +1,35 @@
+package leet;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Q6_ZigzagConversion {
+    public String convert(String s, int numRows) {
+        if (numRows == 1) {
+            return s;
+        }
+
+        numRows = Math.min(numRows, s.length());
+        List<StringBuilder> rows = new ArrayList<>();
+        for (int i = 0; i < numRows; i++) {
+            rows.add(new StringBuilder());
+        }
+
+        int currRow = 0;
+        boolean goingDown = false;
+
+        for (char c : s.toCharArray()) {
+            rows.get(currRow).append(c);
+            if (currRow == 0 || currRow == numRows - 1) {
+                goingDown = !goingDown;
+            }
+            currRow += goingDown ? 1 : -1;
+        }
+
+        StringBuilder res = new StringBuilder();
+        for (StringBuilder row : rows) {
+            res.append(row);
+        }
+        return res.toString();
+    }
+}
