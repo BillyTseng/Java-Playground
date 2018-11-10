58f134c2769166aad4f6ab0ae9eb188b369a20bd
Billy Tseng <tseng.billy@gmail.com>
2018-11-10 09:12:59 -0700

    feat: re-write reverse.

--- src/leet/Q25_ReverseNodesInKGroup.java
+++ src/leet/Q25_ReverseNodesInKGroup.java
@@ -32,17 +32,34 @@ public class Q25_ReverseNodesInKGroup {
         return dummy.next;
     }
 
+    /**
+     * Example:
+     * dummy -> 1 -> 2 -> 3 -> 4 -> 5 -> null, k = 3
+     * start                  end
+     * target list 1->2->3
+     * result list 3->2->1
+     * return lastNode = 1
+     *
+     * @param start the start node which is exclude to the target list
+     * @param end the end node which is exclude to the target list
+     * @return the last node of new list
+     */
+
     private ListNode reverse(ListNode start, ListNode end) {
-        ListNode last = start.next;
-        ListNode curr = last.next;
-
-        while (curr != end) {
-            last.next = curr.next;
-            curr.next = start.next;
-            start.next = curr;
-            curr = last.next;
+        ListNode head = start.next;
+        // save the last node, which is the original head
+        ListNode lastNode = head;
+        // de-link start
+        start.next = null;
+        ListNode prev = end;
+        while (head != end) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
         }
-
-        return last;
+        // link start with new head
+        start.next = prev;
+        return lastNode;
     }
 }
