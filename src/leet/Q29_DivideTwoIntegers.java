33bbeafcd88d4446b1d7cf08689a7e9a8745059b
Billy Tseng <tseng.billy@gmail.com>
2018-11-10 22:00:46 -0700

    feat: algorithm problems.

---
+++ src/leet/Q29_DivideTwoIntegers.java
@@ -0,0 +1,31 @@
+package leet;
+
+public class Q29_DivideTwoIntegers {
+    public int divide(int dividend, int divisor) {
+        if (divisor == 0)
+            return Integer.MAX_VALUE;
+        if (divisor == -1 && dividend == Integer.MIN_VALUE)
+            return Integer.MAX_VALUE;
+
+        long pDividend = Math.abs((long)dividend);
+        long pDivisor = Math.abs((long)divisor);
+
+        int res = 0;
+        while (pDividend >= pDivisor) {
+            int numShift = 0;
+            while (pDividend >= (pDivisor << numShift)) {
+                numShift++;
+            }
+            numShift--;
+
+            res += 1 << numShift;
+            pDividend -= pDivisor << numShift;
+        }
+
+        if ((dividend > 0 && divisor > 0) || (dividend < 0 && divisor < 0)) {
+            return res;
+        } else {
+            return -res;
+        }
+    }
+}
