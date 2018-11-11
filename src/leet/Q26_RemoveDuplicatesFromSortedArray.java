c3023f8e3379b7e31216e5307e3a12b7b3fbd5b0
Billy Tseng <tseng.billy@gmail.com>
2018-11-10 17:13:28 -0700

    feat: algorithm problems.

---
+++ src/leet/Q26_RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,19 @@
+package leet;
+
+public class Q26_RemoveDuplicatesFromSortedArray {
+    public int removeDuplicates(int[] nums) {
+        if (nums == null) {
+            return 0;
+        }
+        if (nums.length <= 1) {
+            return nums.length;
+        }
+        int slow = 1;
+        for (int fast = 1; fast < nums.length; fast++) {
+            if (nums[fast] != nums[slow - 1]) {
+                nums[slow++] = nums[fast];
+            }
+        }
+        return slow;
+    }
+}
