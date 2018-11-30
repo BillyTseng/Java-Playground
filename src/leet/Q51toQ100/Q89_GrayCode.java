0f7d4ec612931c884571ba9ba6104845d2c94f9a
Billy Tseng <tseng.billy@gmail.com>
2018-11-29 22:34:15 -0700

    feat: algorithm problems.

---
+++ src/leet/Q51toQ100/Q89_GrayCode.java
@@ -0,0 +1,23 @@
+package leet.Q51toQ100;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Q89_GrayCode {
+    public List<Integer> grayCode(int n) {
+        if (n == 0) {
+            List<Integer> result = new ArrayList<Integer>();
+            result.add(0);
+            return result;
+        }
+
+        List<Integer> result = grayCode(n - 1);
+        int numToAdd = 1 << (n - 1);
+
+        for (int i = result.size() - 1; i >= 0; i--) {
+            result.add(numToAdd + result.get(i));
+        }
+
+        return result;
+    }
+}
