381fe8659e5cfde43b4dfb08243b70a2ccb8d0c2
Billy Tseng <tseng.billy@gmail.com>
2018-12-03 21:59:58 -0700

    feat: algorithm problems.

---
+++ src/leet/Q51toQ100/Q98_ValidateBinarySearchTree.java
@@ -0,0 +1,27 @@
+package leet.Q51toQ100;
+
+public class Q98_ValidateBinarySearchTree {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    private boolean isValidBST(TreeNode root, long min, long max) {
+        if (root == null) {
+            return true;
+        }
+        if (root.val <= min || max <= root.val) {
+            return false;
+        }
+        return isValidBST(root.left, min, root.val) && isValidBST(root.right, root.val, max);
+    }
+}
