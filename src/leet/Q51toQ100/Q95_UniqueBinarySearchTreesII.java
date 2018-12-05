c5174f56d657616c6cc8da875253b075ab01ce2b
Billy Tseng <tseng.billy@gmail.com>
2018-12-04 21:22:23 -0700

    feat: algorithm problems.

---
+++ src/leet/Q51toQ100/Q95_UniqueBinarySearchTreesII.java
@@ -0,0 +1,43 @@
+package leet.Q51toQ100;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Q95_UniqueBinarySearchTreesII {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public List<TreeNode> generateTrees(int n) {
+        return generateTrees(1, n);
+    }
+
+    private List<TreeNode> generateTrees(int start, int end) {
+        List<TreeNode> res = new ArrayList<>();
+        if (start > end) {
+            res.add(null);
+            return res;
+        }
+
+        for (int i = start; i <= end; i++) {
+            List<TreeNode> left = generateTrees(start, i - 1);
+            List<TreeNode> right = generateTrees(i + 1, end);
+
+            for (TreeNode l : left) {
+                for (TreeNode r : right) {
+                    TreeNode root = new TreeNode(i);
+                    root.left = l;
+                    root.right = r;
+                    res.add(root);
+                }
+            }
+        }
+        return res;
+    }
+}
